c1c8acfcc3606cb80921d9f8d862123782f46f71
DragonLLNG <thung2@uncc.edu>
2023-03-06 15:31:57 -0500

    Sign Up - Sprint 1

--- app/src/main/java/com/example/nailpolishapp/CreateAccountFragment.java
+++ app/src/main/java/com/example/nailpolishapp/CreateAccountFragment.java
@@ -1,8 +1,10 @@
 package com.example.nailpolishapp;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 
@@ -20,11 +23,13 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
 
 
 public class CreateAccountFragment extends Fragment {
-    private FirebaseAuth mAuth;
+
     private final String TAG="final";
     public CreateAccountFragment() {
         // Required empty public constructor
@@ -43,11 +48,12 @@ public class CreateAccountFragment extends Fragment {
         binding = FragmentCreateAccountBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.buttonCancel.setOnClickListener(new View.OnClickListener() {
+  /*      binding.buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mListener.login();
@@ -100,7 +106,117 @@ public class CreateAccountFragment extends Fragment {
         });
 
         getActivity().setTitle("Create New Account");
+*/
 
+
+        binding.buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.login();
+            }
+        });
+
+
+        binding.buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.editTextEmail.getText().toString();
+                String password = binding.editTextPassword.getText().toString();
+                String name = binding.editTextName.getText().toString();
+
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
+
+                if (email.isEmpty()) {
+                    alertBuilder.setTitle(R.string.error)
+                            .setMessage("Email is required")
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Log.d(TAG, "onClick: ");
+                                }
+                            });
+                    alertBuilder.create().show();
+                } else if (password.isEmpty()) {
+                    alertBuilder.setTitle(R.string.error)
+                            .setMessage("Password is at least 8 character")
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Log.d(TAG, "onClick: ");
+                                }
+                            });
+                    alertBuilder.create().show();
+                } else if (name.isEmpty()) {
+                    alertBuilder.setTitle(R.string.error)
+                            .setMessage("Name is required")
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Log.d(TAG, "onClick: ");
+                                }
+                            });
+                    alertBuilder.create().show();
+                } else {
+
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                        .setDisplayName(name)
+                                        .build();
+                                user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+
+                                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                                            HashMap<String, Object> data = new HashMap<>();
+                                            data.put("name", name);
+                                            data.put("uid", mAuth.getCurrentUser().getUid());
+                                            data.put("isOnline", true);
+
+                                            db.collection("users").document(mAuth.getCurrentUser().getUid()).set(data)
+                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            mListener.gotoMenu();
+                                                        }
+                                                    });
+                                        } else {
+                                            alertBuilder.setTitle(R.string.error)
+                                                    .setMessage("Error in creating user! Try again!")
+                                                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                                            Log.d(TAG, "onClick: ");
+                                                        }
+                                                    });
+                                        }
+                                    }
+                                });
+                            //Duplicate email
+                            } else {
+                                alertBuilder.setTitle(R.string.error)
+                                        .setMessage("Sign up failed due to duplicate email")
+                                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                Log.d(TAG, "onClick: ");
+                                            }
+                                        });
+                                alertBuilder.create().show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+
+        getActivity().setTitle("Sign Up");
     }
 
     CreateAccountListener mListener;
