7b37bbfb61ecfdd1f50d08a91c812c20fb30dbe6
DragonLLNG <thung2@uncc.edu>
2023-03-21 23:13:01 -0400

    Sprint 2 - Welcome Screen

---
+++ app/src/main/java/com/example/nailpolishapp/WelcomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.nailpolishapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    Button switchToMain;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        switchToMain = findViewById(R.id.buttonStart);
+        switchToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
+
+    }
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(switchActivityIntent);
+    }
+}
