12efa00b1715159d3d2968fb43e01972363535a4
DragonLLNG <thung2@uncc.edu>
2023-03-21 23:46:44 -0400

    Sprint 2 - Create Polish class and Add Polish Fragment

--- app/src/main/java/com/example/nailpolishapp/AddOnFragment.java
+++ app/src/main/java/com/example/nailpolishapp/AddOnFragment.java
@@ -1,12 +1,21 @@
 package com.example.nailpolishapp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
@@ -19,32 +28,45 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-
 import com.example.nailpolishapp.databinding.FragmentAddOnBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.UUID;
+
+import com.google.firebase.firestore.OnProgressListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class AddOnFragment extends Fragment {
+    private final String TAG="Addon";
 
     FragmentAddOnBinding binding;
+    Bitmap selectedImageBitmap;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    DocumentReference polishDocRef = FirebaseFirestore.getInstance()
+            .collection("Polish").document(mAuth.getCurrentUser().getUid());
+    DocumentReference polishDetailDocRef = FirebaseFirestore.getInstance()
+            .collection("Polish").document(mAuth.getCurrentUser().getUid())
+            .collection("PolishDetail").document();
+    Polish polish = new Polish();
+    private Uri selectedImageUri;
 
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
     public AddOnFragment() {
         // Required empty public constructor
@@ -82,6 +104,144 @@ public class AddOnFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Add Polish");
+
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
+
+        binding.buttonAddOnPolish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String polishName = binding.editTextPolishName.getText().toString();
+
+
+                ArrayList<Polish> polishArrayList = new ArrayList<>();
+
+
+                polish.setId(polishDetailDocRef.getId());
+
+
+                if (polishName.isEmpty()) {
+                    alertBuilder.setTitle(R.string.error)
+                            .setMessage("Please enter a polish name")
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Log.d(TAG, "onClick: ");
+                                }
+                            });
+                    alertBuilder.create().show();
+                } else
+                {
+                    polish.setName(polishName);
+                    polish.setCreatedAt(new Date());
+
+
+                    ArrayList<String> polishType = new ArrayList<>();
+
+                    if (!binding.checkboxButtonRegular.isChecked() &&
+                            !binding.checkboxButtonGel.isChecked() &&
+                            !binding.checkboxButtonDipping.isChecked()) {
+
+                        alertBuilder.setTitle(R.string.error)
+                                .setMessage("What type of polish is it?")
+                                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Log.d(TAG, "onClick: ");
+                                    }
+                                });
+                        alertBuilder.create().show();
+                    } else {
+                        if (binding.checkboxButtonRegular.isChecked()) {
+                            polishType.add("Regular");
+                        }
+                        if (binding.checkboxButtonGel.isChecked()) {
+                            polishType.add("Gel");
+                        }
+                        if (binding.checkboxButtonDipping.isChecked()) {
+                            polishType.add("Dipping Powder");
+                        }
+
+                        polish.setType(polishType);
+                        polish.setUserID(mAuth.getCurrentUser().getUid());
+
+                        polishArrayList.add(polish);
+
+
+                        HashMap<String, Object> polishData = new HashMap<>();
+
+                        polishData.put("userId", mAuth.getCurrentUser().getUid());
+                        polishData.put("polishList", polishArrayList);
+
+
+                        polishDocRef.set(polishData).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+
+                                    polishDetailDocRef.set(polish).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+
+
+                                                alertBuilder.setTitle("Success")
+                                                        .setMessage("Add on successful")
+                                                        .setPositiveButton("Add more", new DialogInterface.OnClickListener() {
+                                                            @Override
+                                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                                mListener.gotoAddOn();
+
+                                                            }
+                                                        })
+                                                        .setNegativeButton("List", new DialogInterface.OnClickListener() {
+                                                            @Override
+                                                            public void onClick(DialogInterface dialog, int which) {
+                                                                mListener.gotoList();
+                                                            }
+                                                        });
+                                                alertBuilder.create().show();
+
+
+                                            } else {
+                                                Toast.makeText(getActivity(), "Error creating a Polish instance", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                                    Log.d("Polish", "onComplete: " + polish.name);
+                                } else {
+                                    Toast.makeText(getActivity(), "Error creating a Polish instance", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+
+
+
+
+                }
+
+
+
+            }
+
+        });
+
+
+        binding.imageViewUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                launchSomeActivity.launch(i);
+
+            }
+        });
+
+
         //Cancel button
         binding.buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,13 +250,33 @@ public class AddOnFragment extends Fragment {
             }
         });
 
-        getActivity().setTitle("Add Polish");
 
     }
+    ActivityResultLauncher<Intent> launchSomeActivity = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            Intent data = result.getData();
+            if (data != null && data.getData() != null) {
+                selectedImageUri = data.getData();
 
+                // uploadImage();
+                Log.d("Image", ": " + selectedImageUri);
+                try {
+                    selectedImageBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImageUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
+                binding.imageViewUpload.setImageBitmap(selectedImageBitmap);
 
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                selectedImageBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                String imageEncoded = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
 
+                polish.imageURL = imageEncoded;
+
+            }
+        }
+    });
 
     AddOnListener mListener;
 
--- app/src/main/java/com/example/nailpolishapp/Polish.java
+++ app/src/main/java/com/example/nailpolishapp/Polish.java
@@ -1,9 +1,98 @@
 package com.example.nailpolishapp;
 
+import android.net.Uri;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FieldValue;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Polish implements Serializable{
+    String userID, name, id, comment;
+    String imageURL;
+    ArrayList<String> type;
+    Date createdAt;
+    boolean liked;
 
+    public Polish() {
+    }
+
+    public Polish(String userID, String name, String id, String imageURL,
+                  String comment, ArrayList<String> type, Date createdAt, boolean liked) {
+        this.userID = userID;
+        this.name = name;
+        this.id = id;
+        this.imageURL = imageURL;
+        this.comment = comment;
+        this.type = type;
+        this.createdAt = createdAt;
+        this.liked = liked;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ArrayList<String> getType() {
+        return type;
+    }
+
+    public void setType(ArrayList<String> type) {
+        this.type = type;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isLiked() {
+        return liked;
+    }
+
+    public void setLiked(boolean liked) {
+        this.liked = liked;
+    }
 }
